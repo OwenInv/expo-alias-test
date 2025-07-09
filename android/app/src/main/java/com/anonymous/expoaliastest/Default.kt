73b7aea751dac15b6d7988a522ecdc7d73eecb6b
Owen <196177287+OwenInv@users.noreply.github.com>
2025-07-09 07:04:16 +0200

    Initial commit

---
+++ android/app/src/main/java/com/anonymous/expoaliastest/Default.kt
@@ -0,0 +1,7 @@
+package com.anonymous.expoaliastest
+
+class Default: MainActivity() {
+    // This class is intentionally left empty.
+    // It serves as a placeholder for the MainActivity class.
+    // The actual implementation is in MainActivity.kt.
+}
